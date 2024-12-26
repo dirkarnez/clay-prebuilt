c01541d996e1aee72e986bc78f30178cbc4cbd85
Dirk Arnez <smalldirkalex@gmail.com>
2024-12-26 22:20:12 +0800

    - upload files

---
+++ local-build.cmd
@@ -0,0 +1,28 @@
+@REM run as Administrator
+@echo off
+
+set DOWNLOADS_DIR=%USERPROFILE%\Downloads
+set DOWNLOADS_DIR_LINUX=%DOWNLOADS_DIR:\=/%
+
+SET PATH=^
+%DOWNLOADS_DIR%\PortableGit\bin;^
+%DOWNLOADS_DIR%\winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1\mingw64;^
+%DOWNLOADS_DIR%\winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1\mingw64\bin;^
+%DOWNLOADS_DIR%\cmake-3.29.0-windows-x86_64\bin;
+
+@REM set PATH=^
+@REM D:\Softwares\winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1\mingw64;^
+@REM D:\Softwares\winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1\mingw64\bin;^
+@REM D:\Softwares\cmake-3.23.0-rc1-windows-x86_64\bin;
+
+cmake.exe -G"MinGW Makefiles" ^
+-DCMAKE_BUILD_TYPE=Release ^
+-DBUILD_SHARED_LIBS=OFF ^
+-DCMAKE_INSTALL_PREFIX="cmake-build/cmake-installation" ^
+-B./cmake-build &&^
+cd cmake-build &&^
+cmake --build . --config Release &&^
+cmake --install . &&^
+echo "Successful build"
+pause
+            
